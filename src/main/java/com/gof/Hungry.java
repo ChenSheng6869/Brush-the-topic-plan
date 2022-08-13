7711f5edff743b733980567ee4085e0bf1245953
chenhengfei <821132332@qq.com>
2022-08-13 14:27:04 +0800

    add HUNGRY mode

--- src/main/java/com/gof/Hungry.java
+++ src/main/java/com/gof/Hungry.java
@@ -1,19 +1,26 @@
 package com.gof;
 
 /**
- * @description: 饿汉式单例模式
+ * @description: DCL单例模式
  * @className: Hungry
  * @author: Chenhf
  * @date: 2022/7/15 16:24
  * @version: 1.0
  */
 public class Hungry {
-
+    private static Hungry HUNGRY;
     private Hungry(){
-
     }
-    private final static Hungry HUNGRY = new Hungry();
-    public static Hungry getInstance(){
+
+    public static Hungry getHUNGRY(){
+        if (HUNGRY == null){
+            synchronized (Hungry.class){
+                if (HUNGRY == null){
+                    HUNGRY = new Hungry();
+                    return HUNGRY;
+                }
+            }
+        }
         return HUNGRY;
     }
 }
